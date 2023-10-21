b212d822f430532546017679ab812721fcd07de7
nafisaali0 <nafisaali20006@gmail.com>
2023-10-21 07:14:58 +0600

    set up delete logic

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    // create DB for product
+    // create DB for store all products
     const productCollection = client.db("productStore").collection("products");
-    // create DB for cart product
+    // create DB for store cart product
     const cartCollection = client
       .db("productStore")
       .collection("cartCollection");
 
-    //receive product from client or create new product
+    //receive product from client or create new product using post method
     app.post("/product", async (req, res) => {
       const newProduct = req.body;
       console.log(newProduct);
@@ -44,14 +44,26 @@ async function run() {
       res.send(result);
     });
 
-    //now read new product
+    //read or get all products using get method
     app.get("/product", async (req, res) => {
       const cursor = productCollection.find();
+
+      // send data to DB in array formet
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    // get data read by id
+    //read or get all products by specific brand using get method
+    app.get("/product/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brand: brand };
+
+      // send data to DB in array formet
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //read or get specific products by id using get method
     app.get("/product/id/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -59,16 +71,12 @@ async function run() {
       res.send(result);
     });
 
-    // get data read by brand name
-    app.get("/product/:brand", async (req, res) => {
-      const brand = req.params.brand;
-      const query = { brand: brand };
-      const result = await productCollection.find(query).toArray();
-      res.send(result);
-    });
+    // update products info
     
-    //  card DB
-    //receive cart product from client
+
+    //cart DB : when client click (add to cart) button
+
+    //receive or create cart product from client
     app.post("/cart", async (req, res) => {
       const cartNewProduct = req.body;
       console.log(cartNewProduct);
@@ -77,10 +85,33 @@ async function run() {
       const result = await cartCollection.insertOne(cartNewProduct);
       res.send(result);
     });
-    //now read cart product
+
+    //read or get all cart products using get method
     app.get("/cart", async (req, res) => {
       const cursor = cartCollection.find();
+
+      // send data to DB
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //read or get specific crat products by id using get method
+    app.get("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      // send data to DB
+      const result = await cartCollection.findOne(query);
+      res.send(result);
+    });
+
+    // delete crat data from DB and server also client using delete method
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      // send data to DB
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
